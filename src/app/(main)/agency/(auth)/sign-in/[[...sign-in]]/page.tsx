40eead069b99cffba94d72b3cdc4afaf3d02f587
yashdev9274 <yashdev.yvd@gmail.com>
2024-02-13 12:38:28 +0530

    Add authentication features and layout and sign-in/sign-up pages

---
+++ src/app/(main)/agency/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignIn } from '@clerk/nextjs'
+import React from 'react'
+
+function Page() {
+  return (
+    <SignIn/>
+  )
+}
+
+export default Page
---
+++ src/app/(main)/agency/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+function Page() {
+  return (
+    <SignUp/>
+  )
+}
+
+export default Page
