2fc1c0ec00cda4b20172bef689b28b6ff0b8d569
yashdev9274 <yashdev.yvd@gmail.com>
2024-02-14 12:55:50 +0530

    Update text styling in Home component

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
         <div className="
           absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"
         />
-        <p className='text-2xl italic text-center md:text-[100px]'> Run your agency at one place</p>
+        <p className='text-2xl italic text-center '> Run your agency at one place</p>
         <div className='bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative'>
           <h1 className='text-5xl font-bold text-center md:text-[300px]'>SoFlow</h1>
         </div>
