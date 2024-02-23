e503cc0ead1ca66fc97f29b12792d2e03b4f57f5
kartikver15gr8 <vermakartikey786@gmail.com>
2024-02-24 00:20:59 +0530

    checkpoint

--- src/app/Landing/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function page() {
-    return (
-        <div>
-            <h1>landing</h1>
-        </div>
-    )
-};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,21 +16,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    
 
-      <html lang="en" suppressHydrationWarning>
-        <body className={font.className}>
 
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    
+    <html lang="en" suppressHydrationWarning>
+      <body className={font.className}>
+
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+
   );
 }
