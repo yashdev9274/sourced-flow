848a3f8409e122d5153d2d4f3feb0de08d93c599
yashdev9274 <yashdev.yvd@gmail.com>
2024-03-02 23:36:11 +0530

    Update agency page, create AgencyDetails function and add form and label components

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -38,7 +38,7 @@ const Page = async ({
   return (
     <div className="flex justify-center items-center mt-4">
       <div className="max-w-[850px] border-[1px] p-4 rounded-xl">
-        <h1 className="text-4xl"> Create An Agency</h1>
+        <h1 className="text-4xl"> Create An Company Account</h1>
         <AgencyDetails
           data={{ companyEmail: authUser?.emailAddresses[0].emailAddress }}
         />
