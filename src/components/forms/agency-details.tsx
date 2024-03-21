f5d3fbd1bcc61c0dae42fca8abbdc80c16efd3a6
yashdev9274 <yashdev.yvd@gmail.com>
2024-03-21 21:06:20 +0530

    improve indentation

--- src/components/forms/agency-details.tsx
+++ src/components/forms/agency-details.tsx
@@ -1,21 +1,23 @@
 "use client"
 
 import React, { use, useEffect, useState } from 'react'
-import { useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { AlertDialog } from '../ui/alert-dialog'
 import { Agency } from '@prisma/client'
 import { useToast } from '../ui/use-toast'
-import { Card, CardContent,
+import {
+    Card, CardContent,
     CardDescription,
     CardHeader,
-    CardTitle, } from '../ui/card'
+    CardTitle,
+} from '../ui/card'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/navigation'
 import { Form } from '../ui/form'
 
-type Props={
-data?: Partial<Agency>
+type Props = {
+    data?: Partial<Agency>
 }
 
 const FormSchema = z.object({
@@ -29,55 +31,54 @@ const FormSchema = z.object({
     state: z.string().min(1),
     country: z.string().min(1),
     agencyLogo: z.string().min(1),
-  })
+})
 
-const  AgencyDetails=({data}: Props)=> {
-    const {toast} = useToast();
+const AgencyDetails = ({ data }: Props) => {
+    const { toast } = useToast();
     const router = useRouter()
     const [deleteAgency, setDeletingAgecny] = useState(false)
     const form = useForm<z.infer<typeof FormSchema>>({
         mode: 'onChange',
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
-      name: data?.name,
-      companyEmail: data?.companyEmail,
-      companyPhone: data?.companyPhone,
-      whiteLabel: data?.whiteLabel || false,
-      address: data?.address,
-      city: data?.city,
-      zipCode: data?.zipCode,
-      state: data?.state,
-      country: data?.country,
-      agencyLogo: data?.agencyLogo,
-    },
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            name: data?.name,
+            companyEmail: data?.companyEmail,
+            companyPhone: data?.companyPhone,
+            whiteLabel: data?.whiteLabel || false,
+            address: data?.address,
+            city: data?.city,
+            zipCode: data?.zipCode,
+            state: data?.state,
+            country: data?.country,
+            agencyLogo: data?.agencyLogo,
+        },
     })
 
-useEffect(() => {
-    if(data)
-    {
-        form.reset(data)
-    }
-},[data])
+    useEffect(() => {
+        if (data) {
+            form.reset(data)
+        }
+    }, [data])
 
-  return (
-    <AlertDialog>
-        <Card>
-            <CardHeader>
-                <CardTitle>
-                    Company Information
-                </CardTitle>
-                <CardDescription>
-                    Lets create your account for your company.
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Form {...form}>
-                    <form></form>
-                </Form>
-            </CardContent>
-        </Card>
-    </AlertDialog>
-  )
+    return (
+        <AlertDialog>
+            <Card>
+                <CardHeader>
+                    <CardTitle>
+                        Company Information
+                    </CardTitle>
+                    <CardDescription>
+                        Lets create your account for your company.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                        <form></form>
+                    </Form>
+                </CardContent>
+            </Card>
+        </AlertDialog>
+    )
 }
 
 export default AgencyDetails
