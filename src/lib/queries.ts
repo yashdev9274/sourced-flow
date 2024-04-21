b3c8698d181daf58a26217a986aa35f3a71b7a91
yashdev9274 <yashdev.yvd@gmail.com>
2024-04-21 13:12:16 +0530

    Write upsert query for updated agency details

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -217,3 +217,60 @@ export const initUser = async (newUser: Partial<User>) => {
   })
   return userData
 }
+
+export const upsertAgency = async (agency: Agency, price?: Plan) => {
+
+
+  if (!agency.companyEmail) return null
+  try {
+    const agencyDetails = await db.agency.upsert({
+      where: {
+        id: agency.id,
+      },
+      update: agency,
+      create: {
+        users: {
+          connect: { email: agency.companyEmail },
+        },
+        ...agency,
+        SidebarOption: {
+          create: [
+            {
+              name: 'Dashboard',
+              icon: 'category',
+              link: `/agency/${agency.id}`,
+            },
+            {
+              name: 'Launchpad',
+              icon: 'clipboardIcon',
+              link: `/agency/${agency.id}/launchpad`,
+            },
+            {
+              name: 'Billing',
+              icon: 'payment',
+              link: `/agency/${agency.id}/billing`,
+            },
+            {
+              name: 'Settings',
+              icon: 'settings',
+              link: `/agency/${agency.id}/settings`,
+            },
+            {
+              name: 'Sub Accounts',
+              icon: 'person',
+              link: `/agency/${agency.id}/all-subaccounts`,
+            },
+            {
+              name: 'Team',
+              icon: 'shield',
+              link: `/agency/${agency.id}/team`,
+            },
+          ],
+        },
+      },
+    })
+    return agencyDetails
+  } catch (error) {
+    console.log(error)
+  }
+}
