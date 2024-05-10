aeb870ed02b39db85f29a6ef86dcb87832624a56
yashdev9274 <yashdev.yvd@gmail.com>
2024-05-10 15:29:07 +0530

    fix: org layout page

--- src/app/(main)/agency/[agencyId]/layout.tsx
+++ src/app/(main)/agency/[agencyId]/layout.tsx
@@ -1,3 +1,6 @@
+import { verifyAndAcceptInvitation } from '@/lib/queries'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from "next/navigation"
 import React from 'react'
 
 type Props = {
@@ -5,10 +8,28 @@ type Props = {
     params: { agencyId: string }
 }
 
-const layout = ({ children, params }: Props) => {
-    return (
-        <div>layout</div>
+const layout = async ({ children, params }: Props) => {
+
+    const agencyId = await verifyAndAcceptInvitation()
+    const user = await currentUser()
+
+    if (!user) {
+        return redirect("/")
+    }
+
+    if (!agencyId) {
+        return redirect('/agency')
+    }
+
+    if (
+        user.privateMetadata.role !== 'AGENCY_OWNER' &&
+        user.privateMetadata.role !== 'AGENCY_ADMIN'
     )
+        // return <Unauthorized />
+
+        return (
+            <div>layout</div>
+        )
 }
 
 export default layout;
