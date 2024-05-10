fb9c8b5a01d0a8fce9171d434954d1cd3a78d8d9
yashdev9274 <yashdev.yvd@gmail.com>
2024-05-10 15:48:10 +0530

    feat: create  unauthorized access component

---
+++ src/components/unauthorized/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from 'next/link'
+
+type Props = {}
+
+const Unauthorized = (props: Props) => {
+    return (
+        <div className="p-4 text-center h-screen w-screen flex justify-center items-center flex-col">
+            <h1 className="text-3xl md:text-6xl">Unauthorized acccess!</h1>
+            <p>Please contact support or your agency owner to get access</p>
+            <Link
+                href="/"
+                className="mt-4 bg-primary p-2"
+            >
+                Back to home
+            </Link>
+        </div>
+    )
+}
