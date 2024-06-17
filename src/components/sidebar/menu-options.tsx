4659d20bba3734e0900fc674619d74aaf835c9ca
yashdev9274 <yashdev.yvd@gmail.com>
2024-06-17 11:43:03 +0530

    🔧 update: sidebar menu-option, add sidebar items.

--- src/components/sidebar/menu-options.tsx
+++ src/components/sidebar/menu-options.tsx
@@ -28,6 +28,8 @@ import Link from 'next/link'
 import CustomModal from '../global/custom-modal'
 import { useModal } from '@/providers/modal-provider'
 import SubAccountDetails from '../forms/subaccount-details'
+import { Separator } from '../ui/separator'
+import { icons } from '@/lib/constants'
 
 
 type Props = {
@@ -259,6 +261,41 @@ const MenuOptions = ({
 
                         </PopoverContent>
                     </Popover>
+                    <p className="text-muted-foreground text-xs mb-2">MENU LINKS</p>
+                    <Separator className="mb-4" />
+                    <nav className="relative">
+                        <Command className="rounded-lg overflow-visible bg-transparent">
+                            <CommandInput placeholder="Search..." />
+                            <CommandList className="py-4 overflow-visible">
+                                <CommandEmpty>No Results Found</CommandEmpty>
+                                <CommandGroup className="overflow-visible">
+                                    {sidebarOpt.map((sidebarOptions) => {
+                                        let val
+                                        const result = icons.find(
+                                            (icon) => icon.value === sidebarOptions.icon
+                                        )
+                                        if (result) {
+                                            val = <result.path />
+                                        }
+                                        return (
+                                            <CommandItem
+                                                key={sidebarOptions.id}
+                                                className="md:w-[320px] w-full"
+                                            >
+                                                <Link
+                                                    href={sidebarOptions.link}
+                                                    className="flex items-center gap-2 hover:bg-transparent rounded-md transition-all md:w-full w-[320px]"
+                                                >
+                                                    {val}
+                                                    <span>{sidebarOptions.name}</span>
+                                                </Link>
+                                            </CommandItem>
+                                        )
+                                    })}
+                                </CommandGroup>
+                            </CommandList>
+                        </Command>
+                    </nav>
                 </div>
             </SheetContent>
         </Sheet>
