3567bb38907427b103271aafe3572e0988da34d7
yashdev9274 <yashdev.yvd@gmail.com>
2024-06-20 11:56:48 +0530

    ➕ create: types file, added NotificationWithUser type.

---
+++ src/lib/types.ts
@@ -0,0 +1,16 @@
+import { Prisma, Role, Notification } from "@prisma/client"
+
+export type NotificationWithUser =
+    | ({
+        User: {
+            id: string
+            name: string
+            avatarUrl: string
+            email: string
+            createdAt: Date
+            updatedAt: Date
+            role: Role
+            agencyId: string | null
+        }
+    } & Notification)[]
+    | undefined
