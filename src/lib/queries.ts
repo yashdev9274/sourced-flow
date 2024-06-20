4015fe53b165cf9a0aece9a53cb11e74bfcc12e5
yashdev9274 <yashdev.yvd@gmail.com>
2024-06-20 13:45:12 +0530

    🔧 update: create query to updateUser.

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -379,3 +379,18 @@ export const getUserPermissions = async (userId: string) => {
 
   return response
 }
+
+export const updateUser = async (user: Partial<User>) => {
+  const response = await db.user.update({
+    where: { email: user.email },
+    data: { ...user },
+  })
+
+  await clerkClient.users.updateUserMetadata(response.id, {
+    privateMetadata: {
+      role: user.role || 'SUBACCOUNT_USER',
+    },
+  })
+
+  return response
+}
