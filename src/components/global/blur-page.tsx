5f4943fafb0f2f3002ed22823602e3e852fa0453
yashdev9274 <yashdev.yvd@gmail.com>
2024-06-20 10:50:06 +0530

    🛠  update: blur page css to make it look good.

--- src/components/global/blur-page.tsx
+++ src/components/global/blur-page.tsx
@@ -6,7 +6,10 @@ type Props = {
 
 const BlurPage = ({ children }: Props) => {
     return (
-        <div>{children}</div>
+        <div
+            className="h-screen overflow-scroll backdrop-blur-[35px] dark:bg-muted/40 bg-muted/60 dark:shadow-2xl dark:shadow-black  mx-auto pt-24 p-4 absolute top-0 right-0 left-0 botton-0 z-[11]"
+            id="blur-page"
+        >{children}</div>
     )
 }
 
