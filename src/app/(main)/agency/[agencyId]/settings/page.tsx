7ed3bd9b0e5c23642bf973bcaa80ae40e4eeb766
yashdev9274 <yashdev.yvd@gmail.com>
2024-06-20 13:30:11 +0530

    🔧 update: settings with with orgDetails.

--- src/app/(main)/agency/[agencyId]/settings/page.tsx
+++ src/app/(main)/agency/[agencyId]/settings/page.tsx
@@ -1,8 +1,44 @@
+import AgencyDetails from '@/components/forms/agency-details'
+import { db } from '@/lib/db'
+import { currentUser } from '@clerk/nextjs'
 import React from 'react'
 
-const SettingsPage = () => {
+type Props = {
+    params: { agencyId: string }
+}
+
+const SettingsPage = async ({ params }: Props) => {
+
+    const authUser = await currentUser()
+    if (!authUser) return null
+
+    const userDetails = await db.user.findUnique({
+        where: {
+            email: authUser.emailAddresses[0].emailAddress
+        }
+    })
+
+    if (!userDetails) return null
+
+    const agencyDetails = await db.agency.findUnique({
+        where: {
+            id: params.agencyId
+        },
+        include: {
+            SubAccount: true
+        }
+    })
+
+    if (!agencyDetails) return null
+
+    const subAccounts = agencyDetails.SubAccount
+
     return (
-        <div>SettingsPage</div>
+        <div
+            className="flex lg:!flex-row flex-col gap-4"
+        >
+            <AgencyDetails data={agencyDetails} />
+        </div>
     )
 }
 
