07854dd1e424ab3fa712d8aaeb7f046c061cd84e
yashdev9274 <yashdev.yvd@gmail.com>
2024-06-20 13:43:11 +0530

    🌟 feat: update and wrap up settings page with orgDetails and userDetails components.

--- src/app/(main)/agency/[agencyId]/settings/page.tsx
+++ src/app/(main)/agency/[agencyId]/settings/page.tsx
@@ -1,4 +1,5 @@
 import AgencyDetails from '@/components/forms/agency-details'
+import UserDetails from '@/components/forms/user-details'
 import { db } from '@/lib/db'
 import { currentUser } from '@clerk/nextjs'
 import React from 'react'
@@ -38,6 +39,12 @@ const SettingsPage = async ({ params }: Props) => {
             className="flex lg:!flex-row flex-col gap-4"
         >
             <AgencyDetails data={agencyDetails} />
+            <UserDetails
+                type="agency"
+                id={params.agencyId}
+                subAccounts={subAccounts}
+                userData={userDetails}
+            />
         </div>
     )
 }
