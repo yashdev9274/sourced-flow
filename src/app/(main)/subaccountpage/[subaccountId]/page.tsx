561b7d314ca40a1ef9bd49b8cfbb6c63dc573428
yashdev9274 <yashdev.yvd@gmail.com>
2024-06-25 12:38:18 +0530

    🛠  update: removed subaccount and subaccountId pages.

--- src/app/(main)/subaccountpage/[subaccountId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type Props = {}
-
-const page = (props: Props) => {
-    return (
-        <div>SubaccountPage</div>
-    )
-}
--- src/app/(main)/subaccountpage/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-type Props = {}
-
-const SubaccountPage = (props: Props) => {
-    return (
-        <div>SubaccountPage</div>
-    )
-}
