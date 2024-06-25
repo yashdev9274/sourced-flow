62fd33791cd9159b8eda023e18330299506e64a5
yashdev9274 <yashdev.yvd@gmail.com>
2024-06-25 13:50:24 +0530

    ⭐ feat: create subaccount and subaccountId page for respective subaccounts.

---
+++ src/app/(main)/subaccount/[subaccountId]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function subaccountId({ }: Props) {
+    return (
+        <div>subaccountId</div>
+    )
+}
+
+export default subaccountId
---
+++ src/app/(main)/subaccount/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const subaccountPage = () => {
+    return (
+        <div>subaccountPage</div>
+    )
+}
+
+export default subaccountPage
