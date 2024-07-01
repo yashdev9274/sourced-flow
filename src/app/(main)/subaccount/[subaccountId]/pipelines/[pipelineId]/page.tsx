bacf24458edbe779dee1afd41e6c96b90f61c0b5
yashdev9274 <yashdev.yvd@gmail.com>
2024-07-01 12:52:45 +0530

    🔧 update: pipelineId Page.

--- src/app/(main)/subaccount/[subaccountId]/pipelines/[pipelineId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/pipelines/[pipelineId]/page.tsx
@@ -1,10 +1,40 @@
+import { db } from '@/lib/db'
+import {
+    getLanesWithTicketAndTags,
+    getPipelineDetails,
+} from '@/lib/queries'
+import { LaneDetail } from '@/lib/types'
+import { redirect } from 'next/navigation'
 import React from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
-type Props = {}
 
-const PipelineIdPage = (props: Props) => {
+type Props = {
+    params: { subaccountId: string; pipelineId: string }
+}
+
+const PipelineIdPage = async ({ params }: Props) => {
+
+    const pipelineDetails = await getPipelineDetails(params.pipelineId)
+
+    if (!pipelineDetails)
+        return redirect(`/subaccount/${params.subaccountId}/pipelines`)
+
+    const pipelines = await db.pipeline.findMany({
+        where: { subAccountId: params.subaccountId },
+    })
+
+    const lanes = (await getLanesWithTicketAndTags(
+        params.pipelineId
+    )) as LaneDetail[]
     return (
-        <div>PipelineIdPage</div>
+        <Tabs
+            defaultValue="view"
+            className="w-full"
+        >
+            <TabsList className="bg-transparent border-b-2 h-16 w-full justify-between mb-4">
+            </TabsList>
+        </Tabs>
     )
 }
 
