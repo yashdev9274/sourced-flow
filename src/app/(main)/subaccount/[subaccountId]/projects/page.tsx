9bba95c15cc2727e1d2b66b2ca7bf1158e1d0297
yashdev9274 <yashdev.yvd@gmail.com>
2024-07-12 16:51:44 +0530

    ⭐ feat: create tabs in projects page.

--- src/app/(main)/subaccount/[subaccountId]/projects/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/projects/page.tsx
@@ -1,10 +1,23 @@
+import { Tabs, TabsList, TabsContent, TabsTrigger } from '@/components/ui/tabs'
 import React from 'react'
 
 const ProjectPage = () => {
     return (
-        <div>
-            ProjectPage
-        </div>
+        <Tabs
+            defaultValue="view"
+            className="w-full"
+        >
+            <TabsList className="bg-transparent border-b-2 h-7 w-full flex justify-end mb-4 pb-5">
+
+                <div>
+                    <TabsTrigger value="view">Project View</TabsTrigger>
+                    <TabsTrigger value="settings">Settings</TabsTrigger>
+                </div>
+            </TabsList>
+            <TabsContent value="view">
+
+            </TabsContent>
+        </Tabs>
     )
 }
 
