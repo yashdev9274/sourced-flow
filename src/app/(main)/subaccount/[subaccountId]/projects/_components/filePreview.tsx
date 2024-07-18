e43c7741246026bacb7e7a155cfc376e002bad7d
yashdev9274 <yashdev.yvd@gmail.com>
2024-07-18 10:46:42 +0530

    🔧 update: new file button in projects, add description section.

--- src/app/(main)/subaccount/[subaccountId]/projects/_components/filePreview.tsx
+++ src/app/(main)/subaccount/[subaccountId]/projects/_components/filePreview.tsx
@@ -10,6 +10,7 @@ import {
     DialogTrigger,
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 
 function FilePreview() {
@@ -17,35 +18,52 @@ function FilePreview() {
         <div>
 
             <Dialog>
-                <DialogTrigger className='w-20 justify-end' asChild>
-                    <Button className='w-20 bg-black hover:bg-zinc-900 justify-centre mt-3 text-white'>New File</Button>
-                </DialogTrigger>
+                <form >
 
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Create New File</DialogTitle>
-                        <DialogDescription>
-                            <Input
-                                placeholder='Enter File Name'
-                                className='mt-3'
+                    <DialogTrigger className='w-20 justify-end' asChild>
+                        <Button className='w-20 bg-black hover:bg-zinc-900 justify-centre mt-3 text-white'>New File</Button>
+                    </DialogTrigger>
 
-                            />
-                        </DialogDescription>
-                    </DialogHeader>
-                    <DialogFooter className=''>
-                        <DialogClose asChild>
-                            <Button
-                                type='button'
-                                className='bg-black hover:bg-zinc-900 text-white'
-                            >
-                                Create
-                            </Button>
-                        </DialogClose>
-                    </DialogFooter>
-                </DialogContent>
+                    <DialogContent>
+                        <DialogHeader className='text-xl'> Create New File
+                            <DialogTitle className='text-sm'>Name your File</DialogTitle>
+                            <DialogDescription>
+                                <Input
+                                    placeholder='Enter File Name'
+                                    className='mt-3'
+                                    required
+
+                                />
+                            </DialogDescription>
+                            <DialogDescription>
+                                <div className="flex flex-col gap-y-2">
+
+                                    <label>Description</label>
+                                    <Textarea
+                                        placeholder='Describe your file as you want'
+                                        className='mt-3'
+                                        required
+
+                                    />
+                                </div>
+                            </DialogDescription>
+
+                        </DialogHeader>
+                        <DialogFooter className=''>
+                            <DialogClose asChild>
+                                <Button
+                                    type='button'
+                                    className='bg-black hover:bg-zinc-900 text-white'
+                                >
+                                    Create
+                                </Button>
+                            </DialogClose>
+                        </DialogFooter>
+                    </DialogContent>
+                </form>
 
             </Dialog>
-        </div>
+        </div >
     )
 }
 
